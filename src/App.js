1782196b6cc0735c01daabbaf145c23d2f01fa99
jessehu312 <jessehu312@gmail.com>
2021-02-13 10:46:47 -0800

    Resolve Merge Conflicts / Integrate BaseWeb

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import * as React from "react";
-import { FileUploader } from "baseui/file-uploader";
+
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Redirect,
+  Route
 } from "react-router-dom";
 import LoginScreen from "./screens/Login";
 import SignUpScreen from "./screens/SignUp";
 import HomeScreen from "./screens/Home";
 
-const Uploader = () => {
-  const [errorMessage, setErrorMessage] = React.useState("");
-  return <FileUploader errorMessage={errorMessage} />;
-};
+const engine = new Styletron();
+
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={HomeScreen}></Route>
-        <Route exact path='/login' component={LoginScreen}></Route>
-        <Route exact path='/register' component={SignUpScreen}></Route>
-      </Switch>
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+            <Switch>
+              <Route exact path='/' component={LoginScreen}></Route>
+              <Route exact path='/register' component={SignUpScreen}></Route>
+              <Route exact path='/home' component={HomeScreen}></Route>
+            </Switch>
+        </BaseProvider>
+      </StyletronProvider>
     </Router>
   );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import * as React from "react";
+import { FileUploader } from "baseui/file-uploader";
+import { StatefulInput } from 'baseui/input';
+
+const Uploader = () => {
+
+  const upload = (e) => {
+    console.log(e)
+  }
+
+  const [errorMessage, setErrorMessage] = React.useState(
+    ""
+  );
+  return <FileUploader errorMessage={errorMessage} />;
+}
 
 const HomeScreen = () => {
-  return <div>Welcome to StudyPal</div>;
+  return (<div>Welcome to StudyPal <Uploader/></div>);
 };
 
 export default HomeScreen;
